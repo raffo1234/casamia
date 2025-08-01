3897c56670df05ba8282c92addd7d400d0a9c91e
raffo1234 <rhmcord@gmail.com>
2025-08-01 16:39:58 -0500

    feat: add fading effect when load images

--- src/app/components/PropertyFirstImage.tsx
+++ src/app/components/PropertyFirstImage.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function PropertyFirstImage({
   src,
@@ -8,21 +9,37 @@ export default function PropertyFirstImage({
   src: string | undefined;
   title: string;
 }) {
-  if (!src)
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  if (!src) {
     return (
       <div className="animate-pulse bg-gray-100 rounded-xl w-full aspect-[5/4] flex justify-center items-center">
         <Icon icon="solar:gallery-broken" fontSize={32} />
       </div>
     );
+  }
 
   return (
-    <Image
-      src={src}
-      alt={title}
-      title={title}
-      width={350}
-      height={350}
-      className="w-full aspect-[5/4] object-cover rounded-xl bg-gray-100"
-    />
+    <div className="relative w-full aspect-[5/4] rounded-xl overflow-hidden">
+      {!imageLoaded && (
+        <div className="absolute inset-0 animate-pulse bg-gray-100 flex justify-center items-center">
+          <Icon icon="solar:gallery-broken" fontSize={32} />
+        </div>
+      )}
+
+      <Image
+        src={src}
+        alt={title}
+        title={title}
+        width={350}
+        height={350}
+        className={`
+          w-full h-full object-cover
+          transition-opacity duration-500 ease-in-out
+          ${imageLoaded ? "opacity-100" : "opacity-0 invisible"}
+        `}
+        onLoad={() => setImageLoaded(true)}
+      />
+    </div>
   );
 }
