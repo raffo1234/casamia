6dc57df57d76324e5a2601573118591cfd09d9be
raffo1234 <rhmcord@gmail.com>
2025-09-06 21:07:54 -0500

    feat: optimize property page loading

--- src/app/(client)/inmueble/[slug]/page.tsx
+++ src/app/(client)/inmueble/[slug]/page.tsx
@@ -2,78 +2,59 @@ import Property from "@/components/Property";
 import RelatedProperties from "@/components/RelatedProperties";
 import { auth } from "@/lib/auth";
 import { supabase } from "@/lib/supabase";
-import { PropertyType } from "@/types/propertyType";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
-type Params = Promise<{ slug: string[] }>;
+export default async function Page({ params }: { params: { slug: string[] } }) {
+  const propertySlug = params.slug;
 
-export default async function Page({ params }: { params: Params }) {
-  const { slug } = await params;
-  const session = await auth();
-  const userEmail = session?.user?.email;
-
-  const { data: property } = (await supabase
-    .from("property")
-    .select(
-      `
-      id,
-      title,
-      slug,
-      description,
-      state,
-      user_id,
-      size,
-      currency,
-      delivery_at,
-      bathroom_count,
-      phase,
-      price,
-      currency,
-      location,
-      google_map,
-      bedroom_count,
-      company_id,
-      user!property_user_id_fkey (
-        id,
-        name,
-        image_url
-      ),
-      company!property_company_id_fkey (
-        id,
-        name,
-        logo_url
-      ),
-      typology (
-        id,
-        name,
-        description,
-        price,
-        currency,
-        size,
-        stock,
-        bathroom_count,
-        bedroom_count
-      )
-    `
-    )
-    .eq("slug", slug)
-    .single()) as { data: PropertyType };
-
-  if (!property) {
-    console.error("Error fetching item");
-    return { notFound: true };
-  }
-
-  if (!slug || !property) {
+  if (!propertySlug) {
     redirect("/404");
   }
 
+  const [session, { data: property }] = await Promise.all([
+    auth(),
+    supabase
+      .from("property")
+      .select(
+        `
+        *,
+        user:user!property_user_id_fkey (
+          id,
+          name,
+          image_url
+        ),
+        company:company!property_company_id_fkey (
+          id,
+          name,
+          logo_url
+        ),
+        typology (
+          id,
+          name,
+          description,
+          price,
+          currency,
+          size,
+          stock,
+          bathroom_count,
+          bedroom_count
+        )
+      `
+      )
+      .eq("slug", propertySlug)
+      .single(),
+  ]);
+
+  if (!property) {
+    redirect("/404");
+  }
+
+  const userEmail = session?.user?.email;
+
   return (
     <>
-      <Suspense>
-        <Property property={property} userEmail={userEmail} />
-      </Suspense>
+      <Property property={property} userEmail={userEmail} />
       <Suspense>
         <RelatedProperties propertyId={property.id} userEmail={userEmail} />
       </Suspense>
